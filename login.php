3b75dbfa3559cc646c423006a5c2932bf40b0e29
Daehyeon Kim <104838522@student.swin.edu.au>
2025-04-29 01:30:00 +1000

    modified header.inc and login.php

--- login.php
+++ login.php
@@ -2,10 +2,10 @@
 
 <form method="post" action="process.php">
     <label for="username">Username:</label><br>
-    <input type="text" name="username" required><br>
+    <input type="text" name="username" id="username" required><br>
 
     <label for="password">Password:</label><br>
-    <input type="password" name="password" required><br>
+    <input type="password" name="password" id="password" required><br>
 
     <input type="hidden" name="token" value="abc123"><!-- hidden token -->
     <input type="submit" value="Login">
